653bcc6601ff10c7a4f8d2fab58e97751aa749a9
Rohithsv25 <35858637+Rohithsv25@users.noreply.github.com>
2018-01-27 09:23:04 +0530

    Add files via upload

---
+++ Rohith_Excercise/src/examples/conditionals/DoWhile.java
@@ -0,0 +1,17 @@
+package examples.conditionals;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+
+		int count = 20;
+
+		do {
+			System.out.println("The Count is " + count);
+			count++;
+		}while(count < 10); {
+			
+		}
+
+	}
+}
